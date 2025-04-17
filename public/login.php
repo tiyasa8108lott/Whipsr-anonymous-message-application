7fbd66f4c019c8c59264a6eaf713021045462674
tiyasa8108lott <pujapal3010@gmail.com>
2025-04-17 12:59:23 +0530

    Updated register.php and login.php inside the public folder

--- public/login.php
+++ public/login.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['registration_success'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | NGL Clone</title>
+    <title>Login | Whispr</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <style>
--- public/register.php
+++ public/register.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register | NGL Clone</title>
+    <title>Register | Whispr</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <style>
